30311f9ce075fca70b7d68e19b6fc76c5a441278
riya-joshi-simform <riya.j@simformsolutions.com>
2023-07-18 19:42:11 +0530

    Add README and composer.json files for this bundle.

---
+++ vendor/feature-toggle-test-bundle/README.md
@@ -0,0 +1,7 @@
+SimformFeatureToggleTestBundle
+==============================
+
+This bundle helps you easily configure your feature toggling in Symfony5
+by adding some simple tags to twig and extending it's configuration.
+
+It's under developement.
--- vendor/feature-toggle-test-bundle/src/FeatureToggleTestBundle.php
+++ vendor/feature-toggle-test-bundle/src/FeatureToggleTestBundle.php
@@ -4,6 +4,11 @@ namespace Simformsolutions\FeatureToggleTestBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Feature toggling for Symfony5
+ *
+ * @author Riya Joshi | Simform Solutions
+ */
 class FeatureToggleTestBundle extends Bundle
 {
     
